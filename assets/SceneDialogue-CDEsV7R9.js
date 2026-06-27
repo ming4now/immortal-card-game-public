db9d6e9563fd98b1cd0ac85ee4eecb5a05ead593
ming4now <ming4now@users.noreply.github.com>
2026-06-27 11:58:40 +0800

    deploy: publish 05774f7

--- assets/SceneDialogue-CDEsV7R9.js
+++ assets/SceneDialogue-CDEsV7R9.js
@@ -1,4 +1,4 @@
-import{n as e,t}from"./main-aB6mcgCN.js";var n={A:10,B:20,C:30},r=`
+import{n as e,t}from"./main-PluxiulL.js";var n={A:10,B:20,C:30},r=`
 .scene-dialogue-toast {
     position: fixed;
     top: 72px;
