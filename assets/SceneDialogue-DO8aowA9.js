bdee434f8a8077dc75dcc3b5ff9ee6b31ae92fb4
ming4now <ming4now@users.noreply.github.com>
2026-06-28 11:36:59 +0800

    deploy: publish 6fd6fcc

--- assets/SceneDialogue-DO8aowA9.js
+++ assets/SceneDialogue-DO8aowA9.js
@@ -1,4 +1,4 @@
-import{n as e,t}from"./main-CLAaxzai.js";var n={A:10,B:20,C:30},r=`
+import{n as e,t}from"./main-BJC3QbW2.js";var n={A:10,B:20,C:30},r=`
 .scene-dialogue-toast {
     position: fixed;
     top: 72px;
